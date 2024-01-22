fc0b3492f98c340317f44a57a0203b46b3dba170
Nana Amoako <63524504+nanadotam@users.noreply.github.com>
2024-01-22 01:52:11 +0000

    Update guessing_game.py

--- Lab/Lab Assignment/guessing_game.py
+++ Lab/Lab Assignment/guessing_game.py
@@ -1,13 +1,19 @@
 """
-This program picks a number at random and allows the the user to guess the correct number.
+This program picks a number at random and 
+allows the the user to guess the correct number.
 """
 
+# Add comments to python code
 import random
 
 def guessing_game(seed: int) -> None:
+    # Set seed for reproducibility
     random.seed(seed)
+    # Generate a random number between 1 and 100
+    # Set the lower and upper bounds for the guessing logic
     lower_bound = 1
     upper_bound = 100
+
 
     print("Welcome to the Guessing Game!")
     secret_number = random.randint(lower_bound, upper_bound)
@@ -18,13 +24,18 @@ def guessing_game(seed: int) -> None:
         guess = int(input("I'm thinking of a number between 1 and 100. Can you guess it?\nGuess: "))
 
         if lower_bound <= guess and guess <= upper_bound:
+            # Incrementing the Attempts Counter
             attempts += 1
+            # Getting the user's guess
             if guess == secret_number:
+                # Displaying a congratulatory message if the guess is correct
                 print(f"You guessed correctly after {attempts} tries! Congratulations.")
                 break
             elif guess < secret_number:
+                # Providing feedback for a low guess
                 print("Good try, but that's too low. Try again.")
             elif guess > secret_number:
+                # Providing feedback for a high guess
                 print("Good try, but that's too high. Try again.")
         else:
             print("Invalid input. Guess a number within the specified range.")
